fa5fbb7d435c9afb1227991153c8b22e05d4c411
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-09-25 21:59:20 -0400

    Updated the subheader with more instructions

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,7 +5,9 @@ function Header() {
   return (
     <div className="header">
       <h1 id="title">Employee Directory</h1>
-      <h3 id="subtitle">Search and Sort</h3>
+      <h3 id="subtitle">
+        Search in the search box or Filter by clicking on the table header
+      </h3>
     </div>
   );
 }
